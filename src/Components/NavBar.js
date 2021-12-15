4d9875be7b6a0c838ea083b0467eb62041ae853a
EliasVerdun02 <95776216+EliasVerdun02@users.noreply.github.com>
2021-12-15 16:52:17 -0300

    Add files via upload
    Profe disculpe el medio de entrega , ya voy a aprender a usar mejor git, Saludos

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            <h1 class="title">SportLine</h1>
+            <ul class="nav">
+                <li><a href="1#">HOMBRE</a></li>
+                <li><a href="1#">MUJER</a></li>
+                <li><a href="1#">OFERTAS</a></li>
+                <li><a href="1#">MARCAS</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
